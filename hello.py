dae2e551e852b1e6e370a31f04e571e259d4db3f
Yesid Vanegas <yesidvanegas68@gmail.com>
2025-01-17 21:20:40 -0500

    Listas en python

---
+++ hello.py
@@ -0,0 +1,13 @@
+print(10 // 3);
+print(10 / 3);
+print(2 ** 2);
+print("Hola" + " yesid")
+print("Hola " * 3)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 and 3 < 4)
+
+name, surname, age = "Yesid", "Vanegas", 25
+
+print("Hola mi nombre es %s %s y tengo %d" %(name, surname, age))
+print(surname.startswith("V"))
