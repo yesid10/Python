b7bb426177f10b136b3579fa7ed1788dd67c26b5
Yesid Vanegas <yesidvanegas68@gmail.com>
2025-01-17 21:54:32 -0500

    Metodos de listas en python

--- 04_lists.py
+++ 04_lists.py
@@ -1,2 +1,22 @@
 my_list = [1, 2, 3, 4, 5]
 print(my_list.index(3))
+print(my_list[-2])
+print(my_list.count(3))
+
+num1, num2, num3, num4, num5 = my_list;
+print(num1)
+
+my_list.append(6)
+print(my_list)
+
+my_list.insert(2, 7)
+print(my_list)
+
+my_list.remove(7)
+print(my_list)
+
+my_list.pop()
+print(my_list.pop(2))
+
+my_list.clear()
+print(my_list)
