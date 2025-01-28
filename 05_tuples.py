497578aefdab29e2e8b106f821d2eaa468c4115a
Yesid Vanegas <yesidvanegas68@gmail.com>
2025-01-28 14:32:44 -0500

    tuplas en python

---
+++ 05_tuples.py
@@ -0,0 +1,20 @@
+my_tuple = tuple();
+print(my_tuple);
+
+my_tuple = (35, 1.77, "Yesid", "Vanegas");
+print(my_tuple);
+print(type(my_tuple));
+
+# print(my_tuple[0]);
+# print(my_tuple[1]);
+# print(my_tuple[-2]);
+
+
+print(my_tuple.count("Yesid"))
+print(my_tuple.index("Vanegas"))
+
+my_other_tuple = (35, 1.77, "Yesid", "Vanegas");
+
+my_sum_tuple = my_tuple + my_other_tuple;
+
+print(my_sum_tuple[0:2])
